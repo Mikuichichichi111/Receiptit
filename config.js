3148d7d6cbda9cbdc6e67de333dc93683330559e
Mikuichichichi111 <noahbernocco@gmail.com>
2026-05-06 19:45:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://fonraobtmkaallsjxtoj.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_QxU-Ql1Mux18PlLjGady5g_gwkTCKSH',
+    IMGBB_API_KEY: 'fcf75efb4dcf86cc32df14a55969f334',
+    ADMIN_PASSWORD: 'receiptit_admin_2024'
+};
